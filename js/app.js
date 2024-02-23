f086b8e5d593fd1452b325b98711f64f9c5e9201
ucisalimah <uci.salimah@pajakku.com>
2024-02-23 14:41:25 +0700

    cors fix

--- js/app.js
+++ js/app.js
@@ -40,6 +40,11 @@ const request = (method, path) => {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials" : "true",
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Methods" : "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            "Access-Control-Allow-Headers" : "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+        
         }
     };
 
